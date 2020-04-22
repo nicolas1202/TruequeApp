eb5c1f706f196b9a80c4dfe7f3b2652fc77985b1
Leonel <mleonelriera@gmail.com>
2020-04-22 15:26:48 -0300

    Splash Screen bar quitada

--- app/src/main/java/com/example/truequeapp/SplashActivity.java
+++ app/src/main/java/com/example/truequeapp/SplashActivity.java
@@ -7,18 +7,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.ProgressBar;
 
 import com.example.truequeapp.ui.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
 
         new Handler().postDelayed(new Runnable() {
             @Override
